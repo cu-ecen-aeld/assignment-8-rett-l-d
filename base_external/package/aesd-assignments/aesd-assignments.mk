84bd8f5290fc644e74de1a92863b7fcb1d981913
rett84 <lucasrettore31@hotmail.com>
2025-11-28 18:08:47 -0500

    Update aesd-assignments.mk

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = f572dd5dc5993d01da3a06180a8920ad40f89de8
+AESD_ASSIGNMENTS_VERSION = dde7239803ac44a00d13cf365ff02b9165f6a7b3
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -16,22 +16,36 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
-    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/aesd-char-driver all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) -C $(@D)/aesd-char-driver clean
 endef
+
+# Specify that the kernel source is in a subdirectory relative to the top of the repo
+AESD_ASSIGNMENTS_MODULE_SUBDIRS = aesd-char-driver
+
+# Ensure the Linux kernel headers are built before trying to build this module
+AESD_ASSIGNMENTS_DEPENDENCIES = linux
 
 # TODO
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
     $(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+
     $(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
+
     $(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdcharload_v1 $(TARGET_DIR)/etc/init.d/S99aesdchar
-	$(INSTALL) -m 0644 $(@D)/aesd-char-driver/aesdchar.ko $(TARGET_DIR)/usr/lib/modules/$(KERNEL_VERSION)/extra/
+
+
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8/drivertest.sh $(TARGET_DIR)/usr/bin/
 endef
 
+# Evaluate the package as a generic package
 $(eval $(generic-package))
+# Evaluate the char driver as a kernel module
+$(eval $(kernel-module))
